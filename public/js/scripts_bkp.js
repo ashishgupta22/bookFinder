2f8eb8dd2e23e5913f15b5f7ea9c03a23fe4e6c6
Ashish Gupta <ashishkv47@gmail.com>
2021-05-11 09:25:59 +0530

    initial commit

---
+++ public/js/scripts_bkp.js
@@ -0,0 +1,82 @@
+function findBook(){
+    var userSearch = document.getElementById('userInput').value;
+    var bookResult = document.getElementById('result');
+
+    bookResult.innerHTML = "";
+
+    $.ajax({
+        type: "GET",
+        url: "https://www.googleapis.com/books/v1/volumes?q=" +userSearch,
+        dataType: "JSON",
+        success: function(book){
+            console.log(book);
+            for(var i = 0; book.items.length; i++){
+                var WrapperDiv = document.createElement('div');
+                WrapperDiv.className = 'media';
+
+                //Create img elements for images
+                var image = document.createElement('img');
+                image.className = 'mr-3';
+                image.src = book.items[i].volumeInfo.imageLinks.thumbnail;
+
+                //Create div element with Class of media-body
+                var div = document.createElement('div');
+                div.className = "media-body";
+                
+                //Create Header For the Body
+                var header = document.createElement('h2');
+                header.className = "mt-1";
+                header.innerHTML = book.items[i].volumeInfo.title;
+
+                //Append header to the body
+                div.appendChild(header);
+                WrapperDiv.appendChild(image);
+                WrapperDiv.appendChild(div);
+                //create h5 element for author
+                var author = document.createElement('h6')
+                author.innerHTML = '<b>Authors:</b>' + ' ' +  book.items[i].volumeInfo.authors;
+                div.appendChild(author);
+                //create paragraph for country
+                var country = document.createElement('p');
+                country.innerHTML = '<b>Country:</b>' + ' ' + book.items[i].accessInfo.country;
+                div.appendChild(country);
+
+                //create paragraph for pageCount
+                var pageCount = document.createElement('p');
+                pageCount.innerHTML = '<b>Total Pages:</b>' + ' ' +  book.items[i].volumeInfo.pageCount;
+                div.appendChild(pageCount);
+
+                //create element for publish date
+                var year = document.createElement('p');
+                year.innerHTML = '<b>Published Date:</b>' + ' ' +  book.items[i].volumeInfo.publishedDate;
+                div.appendChild(year);
+
+                //create element for publisher
+                var publisher = document.createElement('p');
+                publisher.innerHTML = '<b>Publisher:</b>' + ' ' +  book.items[i].volumeInfo.publisher;
+                div.appendChild(publisher)
+
+                //create element for description
+                var description = document.createElement('p');
+                description.innerHTML = book.items[i].volumeInfo.description;
+                div.appendChild(description);
+
+                //create a tag to target book link 
+                var link = document.createElement('a');
+                link.innerHTML = '<b>View more</b>';
+                link.href = book.items[i].volumeInfo.previewLink;
+                link.target = "_blank";
+                div.appendChild(link);
+
+                //Create hr to seperate evry book info
+                var line = document.createElement('hr');
+
+                //Make every elements as children element of bookResult
+                bookResult.appendChild(WrapperDiv);
+                bookResult.appendChild(line);
+
+            }
+            
+        }
+    })
+}
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine("handlebars", exphbs({
+	defaultLayout: "main"
+}));
+
+app.set("view engine", "handlebars");
+
+app.use(express.static('public'));
+
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+	
+	res.render("about.handlebars");
+});
+
+app.get("/about", (req, res) => {
+	
+	res.render("about.handlebars");
+});
+
+app.get("/findBooks", (req, res) => {
+	
+	res.render("contact.handlebars");
+});
+
+app.listen(port, () => {
+
+	console.log("Server is running on port " +port);
+});
